be1feae5a8cef9e4c17b1f62c0943726db47f40a
Nirweint <nirweint_1@mail.ru>
2021-10-14 17:14:58 +0300

    hw1 finish

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import s from './Message.module.css';
 
-function Message() {
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessageType) {
     return (
-        <div>
-
+        <div className={s.message}>
+            <div className={s.avatar}>
+                <img src={props.avatar} alt="avatar"/>
+            </div>
+            <div className={s.cloud}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.messageText}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
