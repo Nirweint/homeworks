66ac4ade4fdd61092379a1c5166291d8c3cc6f1f
Nirweint <nirweint_1@mail.ru>
2021-10-28 16:35:43 +0300

    hw3 done

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,26 +1,34 @@
-import React from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+    name: string // need to fix any
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void // need to fix any
+    addUser: () => void // need to fix any
+    error: string // need to fix any
+    totalUsers: number // need to fix any
+    addUserOnEnter: (e: KeyboardEvent<HTMLInputElement>) => void
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+    {name, setNameCallback, addUser, addUserOnEnter, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error ? `${s.error} ${s.input}` : s.input // need to fix with (?:)
 
     return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
+        <div className={s.greeting}>
+            <div className={s.inputWrapper}>
+                <input
+                    className={inputClass}
+                    value={name}
+                    onChange={setNameCallback}
+                    onKeyPress={addUserOnEnter}
+                />
+                <button className={s.btn} onClick={addUser}>add</button>
+                <span>{totalUsers}</span>
+            </div>
+            <span className={s.errorMessage}>{error}</span>
         </div>
     )
 }
