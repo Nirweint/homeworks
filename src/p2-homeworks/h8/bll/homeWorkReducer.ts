6640dffef0465e71fd2bfd17e9794411a9303cd9
Nirweint <nirweint_1@mail.ru>
2021-12-05 15:45:54 +0300

    hw8 finish

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,22 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: UserType[], action: any): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
             // need to fix
+            const stateCopy = [...state]
+            if (action.payload === 'up') {
+                return stateCopy.sort((a, b) => a.name <= b.name ? -1 : 1)
+            }
+            if (action.payload === 'down') {
+                return stateCopy.sort((a, b) => a.name <= b.name ? 1 : -1)
+            }
             return state
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(p => p.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
